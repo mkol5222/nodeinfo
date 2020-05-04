83551a4699cd1025122ff08ce0b8eaf4c6f56608
Martin Koldovsky <mkoldov@mak.local>
2020-05-04 17:47:56 +0200

    .

---
+++ README.md
@@ -0,0 +1,4 @@
+# NodeInfo 
+
+* inspired by https://nodejs.org/fr/docs/guides/nodejs-docker-webapp/ and https://github.com/openfaas/faas/blob/master/sample-functions/NodeInfo/main.js
+
--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 
 const express = require('express');
 
+let os = require('os');
+let fs = require('fs');
+
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
@@ -9,7 +12,24 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  fs.readFile("/etc/hostname", "utf8", (err, data) => {
+    const resObj = {
+      "Hostname": data,
+      "Platform": os.platform(),
+      "Arch": os.arch(),
+      "CPU count": os.cpus().length,
+      "Uptime": os.uptime(),
+      cpus: os.cpus(),
+      netIf: os.networkInterfaces()
+    }
+    //if (content && content.length && content.indexOf("verbose") > -1) {
+    //res.send(os.cpus());
+    //res.send(os.networkInterfaces());
+    //}
+
+    res.json(resObj);
+  });
+
 });
 
 app.listen(PORT, HOST);
