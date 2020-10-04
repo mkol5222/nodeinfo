2a169fe065c1d3d4d529221f8fc8b83650a97c77
Martin Koldovsky <mkoldov@mak.local>
2020-10-04 22:58:18 +0200

    pgTables, doc

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 
 * inspired by https://nodejs.org/fr/docs/guides/nodejs-docker-webapp/ and https://github.com/openfaas/faas/blob/master/sample-functions/NodeInfo/main.js
 
+## Environment
+
+| Env variable        | Description           | Note  |
+| ------------- |-------------| -----|
+| 
+| DB_ENDPOINT      | Postgres hostname | |
+| DB_USERNAME | Postgres account username | |
+| DB_PASSWORD| Postgres account password | |
+| DB_NAME| Postgres database name | |
+Note: Postgres DB is expected on port 5432
+
+## Changelog
+
+* 04/Oct/2020 - added pgTables controlled by env vars
--- server.js
+++ server.js
@@ -5,13 +5,45 @@ const express = require('express');
 let os = require('os');
 let fs = require('fs');
 
+// Postgres db
+
+// accessing Postgres DB
+const {
+  Pool
+} = require('pg');
+
+// DB engine used - by env vars
+const pool = new Pool({
+  host: process.env.DB_ENDPOINT,
+  port: 5432,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+console.log(`[PG] ${process.env.DB_USERNAME} ${process.env.DB_PASSWORD} ${process.env.DB_NAME}`)
+
+async function pgTables() {
+  try {
+    // test DB
+    const data = await pool.query('SELECT * FROM pg_catalog.pg_tables;');
+    return data.rows.map(t => t.tablename)
+  } catch (err) {
+    console.error("[PG] error", err)
+  }
+  return null;
+}
+
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+
+  const pgTab = await pgTables();
+
   fs.readFile("/etc/hostname", "utf8", (err, data) => {
     const resObj = {
       "Hostname": data,
@@ -20,7 +52,8 @@ app.get('/', (req, res) => {
       "CPU count": os.cpus().length,
       "Uptime": os.uptime(),
       cpus: os.cpus(),
-      netIf: os.networkInterfaces()
+      netIf: os.networkInterfaces(),
+      pgTables: pgTab
     }
     //if (content && content.length && content.indexOf("verbose") > -1) {
     //res.send(os.cpus());
